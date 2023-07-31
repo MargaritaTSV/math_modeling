dc8cd3c88d4a28f4e8adcc14dbab476189b835d7
MargaritaTSV <137421924+MargaritaTSV@users.noreply.github.com>
2023-07-31 15:38:03 +0000

    gotovo

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
-print("Hello, World")
+# import matplotlib.pyplot as plt
+
+# plt.plot([1, 2, 3], [5, 7, 10])
+# plt.savefig('pic_1.png')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [3, 4, 5]
+y = [40, 10, 30]
+
+plt.plot(x, y, color='g', label='luchte', marker='>', ms=5)
+
+#-------Beauty for python--------#
+#copy from presentation
